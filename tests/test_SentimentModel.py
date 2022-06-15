c9345752e71346cbeef0532b616b2fd8c3b7fef1
Moritz Wilksch <moritzwilksch@gmail.com>
2022-06-15 11:55:04 +0000

    fix numpy test

---
+++ tests/test_SentimentModel.py
@@ -0,0 +1,33 @@
+import pytest
+from pyfin_sentiment.model import SentimentModel
+import numpy as np
+
+SentimentModel.download("small")
+model = SentimentModel("small")
+
+
+@pytest.mark.parametrize(
+    "text, prediction",
+    [
+        ("long $AAPL", "1"),
+        ("short $TSLA", "3"),
+        ("Join our chatroom!", "2"),
+    ],
+)
+def test_predict(text: str, prediction: str):
+    pred = model.predict([text])
+    assert pred == np.array([prediction])
+
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        ("long $AAPL"),
+        ("short $TSLA"),
+        ("Join our chatroom!"),
+    ],
+)
+def test_predict_proba(text: str):
+    pred = model.predict_proba([text])
+    assert pred.shape == (1, 3)
+    np.testing.assert_array_almost_equal(pred.sum(axis=1), np.array([1.0]), verbose=True)
