702d9b10fb721671557e08a0cfbd059685aae3f6
Moritz Wilksch <moritzwilksch@gmail.com>
2024-03-17 20:12:48 +0000

    build & bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyfin-sentiment",
-    version="0.1.0",
+    version="0.1.1",
     author="Moritz Wilksch",
     author_email="moritzwilksch@gmail.com",
     description="A library for market sentiment analysis of financial social media posts",
